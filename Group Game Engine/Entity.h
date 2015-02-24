feedcc3757d89e6e47a4fab020703849ae139cba
Monami Yang <monamiy@uci.edu>
2015-02-24 10:52:28 -0800

    adding sprite class

--- Group Game Engine/Entity.h
+++ Group Game Engine/Entity.h
@@ -1,8 +1,16 @@
 #pragma once
+#include "Sprite.h"
+
+
 class Entity
 {
 public:
 	Entity();
 	~Entity();
+
+	// Primary Functions
+	void update();
+	void draw(SDL_Renderer& renderer);
+	void moveSprite(Sprite& sprite, float move_x, float move_y, float angle);
 };
 
