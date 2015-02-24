1c260705534f04aeada51ee4ca2c438b05b4846f
Andrew <aschau@uci.edu>
2015-02-24 00:35:40 -0800

    added Thomas's and Manu's scene additions. Made some minor adjustments but nothing hugely different from what they originally put. Might make some additions

--- Group Game Engine/Scene.cpp
+++ Group Game Engine/Scene.cpp
@@ -1,11 +1,33 @@
+#include <iostream>
+#include <SDL.h>
 #include "Scene.h"
 
-
-Scene::Scene()
+/*Scene::Scene()
 {
+std::vector<Entity>
+}
+don't need this?*/
+
+Scene::Scene(std::vector<Entity> entity)
+{
+	std::copy(entity.begin(), entity.end(), entities.begin());
 }
 
-
-Scene::~Scene()
+int Scene::getSize()
 {
+	return entities.size();
+}
+
+void Scene::addEntity(Entity entity)
+{
+	entities.push_back(entity);
+}
+
+void Scene::delEntity(Entity entity){
+	for (int i = 0; i < entities.size(); i++){
+		if (entity.id() == entities[i].id()){
+			entities.erase(entities.begin() + i);
+		}
+
+	}
 }
