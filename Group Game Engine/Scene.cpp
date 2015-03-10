e2e355fe27f7b92e601cd5b73cbbf9cf0c4523c4
Thomas Lam <lamth@uci.edu>
2015-03-10 15:12:07 -0700

    Couple more changes to it with lots of comments on confused parts(Pablo look over please.)

--- Group Game Engine/Scene.cpp
+++ Group Game Engine/Scene.cpp
@@ -4,17 +4,13 @@
 
 //change everytyhing to using a map.
 Scene::Scene(int sceneWidth, int sceneHeight) : sceneWidth(sceneWidth), sceneHeight(sceneHeight){}
-
-
-//constructor assuming that there's a preexists vector of entities 
-
-//will update the scene. goes through the scene that it's currently in and calls entity.update 
-//	to see whether or not the entity is alive. if it's not, remove it. else, render i. //unsure as to what to pass.
-//	do we need to clear first since this might be doing the rendering?
+//constructor now takes in the sceneWidth and sceneHeight so that it could be initialized correctly. 
+//I'm not sure what these are needed for since I don't really use it throughout. 
 
 
 //i need a way to determine what types of objects they are in order to display specific messages. 
 //Maybe add in an objectID? 1/2/3 for Ship, Bullets, Rocks/Enemy objects? 
+//I did it assuming that i'll there will be an objectID in order to distinguish between the two. 
 void Scene::update()
 {
 	for (std::map<int, Sprite>::iterator it = sprites.begin(); it != sprites.end(); it++)
@@ -26,7 +22,21 @@ void Scene::update()
 			{
 				if (it->second.getX() == it2->second.getX() && it->second.getY() == it2->second.getY())
 					//checking if the coordinates are the same. 
-					delSprite(it->first);
+				{
+					if (it->second.objectID == 1)
+					{	
+						//call messenger for ship death
+					}
+					else if (it->second.objectID == 2)
+					{
+						//call messenger for bullet death
+					}
+					else
+					{	
+						//call messenger for rock/enemy death.
+					}
+					delSprite(it->first);//remove no matter what
+				}
 					//removing the spirte by ID from the map. 
 				
 			}
@@ -37,32 +47,38 @@ void Scene::update()
 
 void Scene::draw()
 {
+	//not really sure as to how you want me to do this anymore. I'm assuming this was goign to render everything.
+	//sprite has two draw functions, but I don't believe that I can use either without some more context. 
+	//i've tried going through the code and other cpps, but here's nothing really helpful hat I could find. 
 	for (std::map<int, Sprite>::iterator it = sprites.begin(); it != sprites.end(); it++)
 	{
 		//call sprite.draw()? need an int in order to do so. Frame index. is there going to be a way to know which index?
 	}
-	//i'm assuming call camera.draw here?
+	//i'm assuming call camera.draw here per your email. However, it's commented out now, so I guess it's no longer needed. 
 }
 
 //adds an entity onto the scene. 
 //need to add a spriteID to the sprite cpp in order for this to work since it's not working with entities.
+//You said to change this from entities to sprites, and that you'd change it so that sprites had IDs in order to work for this.
 
 void Scene::addSprite(Sprite sprite)
 {
 	sprites.insert(std::pair<int, Sprite>(sprite.spriteID, sprite));
-}
+}//adds a sprite into the mapping. Uses it's spriteID in order to map it. 
 
 void Scene::delSprite(int id)
 {
 	sprites.erase(id);
-}
+}//removes a sprite based on it's ID in the mapping.
 
-//removes an entity onto the scene.
+
 Sprite& Scene::getSprite(int id)
 {
 	return sprites.at(id);
-}
+}//get a sprite given it's ID in the mapping
 
+
+//the rest is just testing for multimaps. Multimaps may have been easier, but the deletion was confusing. 
 /*adding for multimap
 void Scene::addSprite(Sprite sprite)
 {
